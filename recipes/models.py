56b2d5c5382de6e29f319df1421e088a4e5cc200
Rodrigo Santos <rodrigom376@gmail.com>
2022-11-25 14:10:05 -0300

    Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -7,10 +12,10 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    
